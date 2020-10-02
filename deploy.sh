406b715fb0b32a338fa65ce5381af7a37d284e06
Christian <christian.salway@sky.uk>
2020-10-02 15:59:43 +0100

    Automated deployment with SAM

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
-source .env  # file should contain:  export AWS_PROFILE=<profile_name>
+source .env  # export {AWS_PROFILE, STACK_NAME, AUTHORITY, CLIENT_ID, CLIENT_SECRET}
 
 rm -rf {deploy,package}
 
@@ -11,4 +12,19 @@ pip install -t ./package -r requirements.txt
 (cd package || exit 1; zip -r9 "${OLDPWD}/deploy/lambda_function" .)
 (cd src || exit 1; zip -rg "${OLDPWD}/deploy/lambda_function" .)
 
-sam deploy --guided
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+S3_BUCKET_NAME="aws-sam-default-sourcebucket-${AWS_ACCOUNT_ID}"
+
+if [[ -z $(aws s3api list-buckets --query "Buckets[?Name=='${S3_BUCKET_NAME}']" --output text) ]]; then
+  aws s3 mb s3://${S3_BUCKET_NAME}
+fi
+
+sam deploy --stack-name "aws-sam-default-${STACK_NAME}" \
+--capabilities CAPABILITY_IAM \
+--parameter-overrides "Authority=\"${AUTHORITY}\" ClientID=\"${CLIENT_ID}\" ClientSecret=\"${CLIENT_SECRET}\"" \
+--s3-bucket ${S3_BUCKET_NAME} \
+--s3-prefix "${STACK_NAME}" \
+--confirm-changeset
+
+# clean up
+rm -rf {deploy,package}
