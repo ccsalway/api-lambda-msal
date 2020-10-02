13afbb551d04c6d09db52827f66bbb0fc7ce7ad9
Christian <christian.salway@sky.uk>
2020-10-02 17:22:07 +0100

    Automated deployment with SAM

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-set -e
+set -e  # halt on error
 
-source .env  # export {AWS_PROFILE, STACK_NAME, AUTHORITY, CLIENT_ID, CLIENT_SECRET}
+# Publishing serverless applications using the AWS SAM CLI
+# https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-template-publishing-applications.html
+
+source .env # export {AWS_PROFILE, STACK_NAME, AUTHORITY, CLIENT_ID, CLIENT_SECRET}
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+S3_BUCKET_NAME="aws-sam-sourcebucket-${AWS_ACCOUNT_ID}"
+PACKAGE_NAME="./deploy/lambda_package.zip"
 
 rm -rf {deploy,package}
 
@@ -9,22 +16,22 @@ mkdir -p {deploy,package}
 
 pip install -t ./package -r requirements.txt
 
-(cd package || exit 1; zip -r9 "${OLDPWD}/deploy/lambda_function" .)
-(cd src || exit 1; zip -rg "${OLDPWD}/deploy/lambda_function" .)
-
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-S3_BUCKET_NAME="aws-sam-default-sourcebucket-${AWS_ACCOUNT_ID}"
+(cd package || exit 1; zip -r9 ".${PACKAGE_NAME}" .)
+(cd src || exit 1; zip -rg ".${PACKAGE_NAME}" .)
 
 if [[ -z $(aws s3api list-buckets --query "Buckets[?Name=='${S3_BUCKET_NAME}']" --output text) ]]; then
   aws s3 mb s3://${S3_BUCKET_NAME}
 fi
 
-sam deploy --stack-name "aws-sam-default-${STACK_NAME}" \
---capabilities CAPABILITY_IAM \
---parameter-overrides "Authority=\"${AUTHORITY}\" ClientID=\"${CLIENT_ID}\" ClientSecret=\"${CLIENT_SECRET}\"" \
---s3-bucket ${S3_BUCKET_NAME} \
---s3-prefix "${STACK_NAME}" \
---confirm-changeset
+sam deploy \
+  --template-file template.yaml \
+  --parameter-overrides "Authority=\"${AUTHORITY}\" ClientID=\"${CLIENT_ID}\" ClientSecret=\"${CLIENT_SECRET}\"" \
+  --stack-name "aws-sam-${STACK_NAME}" \
+  --capabilities "CAPABILITY_IAM" \
+  --s3-bucket "${S3_BUCKET_NAME}" \
+  --s3-prefix "${STACK_NAME}" \
+  --confirm-changeset
 
-# clean up
-rm -rf {deploy,package}
+rm -rf {deploy,package}  # clean-up
+
+echo "Configure the AAD application Redirect URI, Logout URL and Home page URL as per the README."
